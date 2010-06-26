6fd21a0d442b9f3d6093a20e6b87caae142e5c5b
Danijel Tasov <dt@korn.shell.la>
2010-06-26 11:37:18 +0200

    simplified

--- lib/App/Nopaste/Service/Pastedance.pm
+++ lib/App/Nopaste/Service/Pastedance.pm
@@ -4,11 +4,11 @@ use warnings;
 use strict;
 use Encode;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use base q[App::Nopaste::Service];
 
-sub uri { return 'http://pb.rbfh.de/' }
+sub uri { $ENV{PASTEDANCE_URL} || 'http://pb.rbfh.de/' }
 
 
 sub fill_form {
@@ -18,9 +18,8 @@ sub fill_form {
     my $content = {
         code    => decode('UTF-8', $args{text}),
         subject => decode('UTF-8', $args{desc}),
-        lang    => exists( $self->FORMATS->{ $args{lang} } )
-          ? $self->FORMATS->{ $args{lang} }
-          : 'txt',
+        # Pastedance itself defaults to txt if <lang> is not known
+        lang    => $args{lang},
     };
 
     my $form = $mech->form_number(1) || return;
@@ -29,9 +28,9 @@ sub fill_form {
     @{$mech->requests_redirectable} = ();
 
     my $paste = HTML::Form::Input->new(
-        type => 'text',
+        type  => 'text',
         value => 'Send',
-        name => 'paste'
+        name  => 'paste'
     )->add_to_form($form);
 
     return $mech->submit_form( form_number => 1, fields => $content );
@@ -48,55 +47,5 @@ sub return {
     }
 }
 
-sub FORMATS {
-    {
-        ada         => "Ada",
-        applescript => "Applescript",
-        asm         => "Assembler",
-        bib         => "Bib",
-        yacc        => "Bison",
-        shell       => "Bourne Shell",
-        c           => "C",
-        csharp      => "C#",
-        'c++'       => "C++",
-        caml        => "CAML",
-        changelog   => "changelog",
-        cobol       => "COBOL",
-        conf        => "conf",
-        css         => "CSS",
-        d           => "D",
-        diff        => "diff",
-        bat         => "DOS Batch",
-        erl         => "Erlang",
-        flex        => "Flex",
-        f77         => "Fortran (Fixed)",
-        f90         => "Fortran (Free)",
-        glsl        => "GLSL",
-        haskell     => "Haskell",
-        html        => "HTML",
-        ini         => "INI",
-        java        => "Java",
-        js          => "Javascript",
-        latex       => "LaTeX",
-        ldif        => "LDIF",
-        lua         => "Lua",
-        m4          => "m4",
-        make        => "Makefile",
-        pascal      => "Pascal",
-        perl        => "Perl",
-        php         => "PHP",
-        ps          => "Postscript",
-        python      => "Python",
-        ruby        => "Ruby",
-        scala       => "Scala",
-        slang       => "Slang",
-        sql         => "SQL",
-        txt         => "txt",
-        tcl         => "tcl/tk",
-        vb          => "VB Script",
-        xml         => "XML",
-        xorg        => "Xorg",
-    };
-}
 
 __END__
